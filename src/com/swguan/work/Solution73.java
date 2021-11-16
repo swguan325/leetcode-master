8f3068b217989fbdfcadb6b6575e4727adf1363d
BruceGuan <41408276+swguan325@users.noreply.github.com>
2021-11-16 23:09:32 +0800

    modify. Solution73 Set Matrix Zeroes
    1. use HashSet

--- src/com/swguan/work/Solution73.java
+++ src/com/swguan/work/Solution73.java
@@ -1,13 +1,43 @@
 package com.swguan.work;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 
  * author: swGuan
  * 
  */
 public class Solution73 {
-			
+	
+	// hash set keep [i][j] = 0
 	public void setZeroes(int[][] matrix) {
+		Set<Integer> rows = new HashSet<Integer>();
+	    Set<Integer> cols = new HashSet<Integer>();
+	    
+	    for(int i = 0; i < matrix.length; i++) {
+	        for(int j = 0; j < matrix[0].length; j++) {
+	            if(matrix[i][j] == 0) {
+	            	rows.add(i);
+	            	cols.add(j);
+	            }
+	        }
+	    }
+	    
+	    for(int row : rows){
+	        for(int j = 0; j < matrix[0].length; j++){
+	            matrix[row][j] = 0;
+	        } 
+	    }
+	    for(int col : cols){
+	        for(int j = 0; j < matrix.length; j++){
+	            matrix[j][col] = 0;
+	        } 
+	    }
+	}
+	
+	// m*n
+	public void setZeroesSlow(int[][] matrix) {
 		int rows = matrix.length;
 		int cols = matrix[0].length;
 		boolean[][] checked = new boolean[rows][cols];
